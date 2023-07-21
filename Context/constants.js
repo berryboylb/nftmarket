e4f3568e1d307999c71ec62d39945866be29becc
berryboylb <phemmynesce4life@gmail.com>
2023-07-21 09:43:19 +0100

    @chore: changes

---
+++ Context/constants.js
@@ -0,0 +1,9 @@
+/// 0x5FbDB2315678afecb367f032d93F642f64180aa3
+/// 0x5FbDB2315678afecb367f032d93F642f64180aa3
+/// 0x5FbDB2315678afecb367f032d93F642f64180aa3
+/// 0x5FbDB2315678afecb367f032d93F642f64180aa3
+import dotenv from "dotenv";
+import nftMarketplace from "./NFTMarketplace.json";
+dotenv.config()
+export const NFTMarketplaceAddress = "0xd01c4Cbd36521ae62f808Ad1f6f34Fc9869B31E1";
+export const nftMarketplaceAbi = nftMarketplace.abi;
