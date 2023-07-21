0a8201fa11fac8b0dbf478b4490707ee428ff36d
berryboylb <phemmynesce4life@gmail.com>
2023-07-21 09:10:46 +0100

    @chore Topcretor

---
+++ TopCreator/TopCreator.js
@@ -0,0 +1,21 @@
+
+export const getTopCreators = (creators) => {
+    // if(!creators) return [];
+    const finalCreators = []
+
+    const finalResult = creators.reduce((index, currentValue) => {
+        (index[currentValue.seller] = index[currentValue.seller] || []).push(
+            currentValue
+        )
+
+        return index;
+    }, {})
+    Object.entries(finalResult).forEach((item) => {
+        const seller = item[0]
+        const total = item[1]
+            .map((newItem) => Number(newItem.price))
+            .reduce((previousValue, currentValue) => previousValue + currentValue, 0)
+        finalCreators.push({ seller, total })
+    })
+    return finalCreators
+}
